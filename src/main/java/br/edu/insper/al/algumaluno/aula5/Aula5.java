ff8073d804889b5544bfc3f2f0ffec0a05f42809
Gabriela Caruso <gabriela.caruso@yahoo.com.br>
2019-08-23 15:16:46 -0300

    parte 5

--- src/main/java/br/edu/insper/al/algumaluno/aula5/Aula5.java
+++ src/main/java/br/edu/insper/al/algumaluno/aula5/Aula5.java
@@ -5,32 +5,25 @@ public class Aula5 {
         Citizen citizen = new Citizen("Frodo");
         Politician lord = new CorruptPolitician("Sauron", 1000000.0);
 
-        Politician politician0 = new Politician("Gandalf", 9000.0);
-        Politician politician1 = new CorruptPolitician("Saruman", 10000.0);
-        Politician politician2 = new Politician("Aragorn", 1000.0);
-        Politician politician3 = new CorruptPolitician("Boromir", 900.0);
+        Politician[] politician = new Politician[4];
 
-        politician0.addNetworking(1);
-        politician1.addNetworking(2);
+        politician[0] = new Politician("Gandalf", 9000.0);
+        politician[1] = new CorruptPolitician("Saruman", 10000.0);
+        politician[2] = new Politician("Aragorn", 1000.0);
+        politician[3] = new CorruptPolitician("Boromir", 900.0);
 
-        System.out.println(politician0.getName() + ": " + politician0.getIncome());
-        politician0.salute(citizen);
-        politician0.salute(lord);
-        System.out.println();
 
-        System.out.println(politician1.getName() + ": " + politician1.getIncome());
-        politician1.salute(citizen);
-        politician1.salute(lord);
-        System.out.println();
 
-        System.out.println(politician2.getName() + ": " + politician2.getIncome());
-        politician2.salute(citizen);
-        politician2.salute(lord);
-        System.out.println();
+        politician[0].addNetworking(1);
+        politician[1].addNetworking(2);
 
-        System.out.println(politician3.getName() + ": " + politician3.getIncome());
-        politician3.salute(citizen);
-        politician3.salute(lord);
-        System.out.println();
+        for (int i = 0; i <= 3; i++){
+            System.out.println(politician[i].getName() + ": " + politician[i].getIncome());
+            politician[i].salute(citizen);
+            politician[i].salute(lord);
+            System.out.println();
+
+        }
+
     }
 }
